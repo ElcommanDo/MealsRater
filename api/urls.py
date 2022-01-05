bac4b116b04e2336eb84e5c6490909134e3369f2
ElcommanDo <abdelrhman2350342@gmail.com>
2022-01-05 23:19:15 +0200

    last commit token authentications

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MealView, RatingView
+from .views import MealView, RatingView, UserViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 router = DefaultRouter()
 router.register('meals', MealView)
 router.register('ratings', RatingView)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', obtain_auth_token),
 ]
--- api/views.py
+++ api/views.py
@@ -1,21 +1,25 @@
-from rest_framework import viewsets
-from .serializers import MealSerializer, RatingSerializer
+from rest_framework import viewsets, status
+from .serializers import MealSerializer, RatingSerializer, UserSerializer
 from .models import Meal, Rating, User
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny, IsAuthenticatedOrReadOnly
 # Create your views here.
 
 
 class MealView(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
+    permission_classes = [IsAuthenticated, ]
+    authentication_classes = [TokenAuthentication]
 
     @action(methods=['POST', ], detail=True)
     def create_rate(self, request, pk):
         if 'stars' in request.data:
             meal = Meal.objects.get(pk=pk)
-            username = request.data['username']
-            user = User.objects.get(username=username)
+            user = request.user
             stars = request.data['stars']
 
             try:
@@ -48,3 +52,38 @@ class RatingView(viewsets.ModelViewSet):
 
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAuthenticated, ]
+
+    def update(self, request, *args, **kwargs):
+        response = {'message':
+                    'this is not the right way to make update',
+                    'status': status.HTTP_400_BAD_REQUEST
+                    }
+        return Response(response)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message':
+                    'this is not the right way to make create',
+                    'status': status.HTTP_400_BAD_REQUEST
+                    }
+        return Response(response)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            instance = serializer.save()
+            instance.set_password(instance.password)
+            instance.save()
+            token, created = Token.objects.get_or_create(user=serializer.instance)
+            response = {'token': token.key, }
+            return Response(response)
+        else:
+            response = {'message': 'something went wrong'}
+            return Response(response)
+
